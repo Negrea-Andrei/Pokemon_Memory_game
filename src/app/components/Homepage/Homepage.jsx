1a5649db50f786075ae71a1e04a79efbdbae716a
Negrea-Andrei <negrea97@yahoo.com>
2023-11-06 19:35:55 +0200

    Create the pokeball homepage

---
+++ src/app/components/Homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import './Homepage.css'
+
+export default function Homepage() {
+  return (
+    <div className="home_page">
+      <div className="top"></div>
+      <div className="middle">
+        <button className="button"></button>
+      </div>
+      <div className="bottom"></div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Homepage from "./components/Homepage/Homepage"
 
 export default function Home() {
   return (
     <>
-    test</>
+      <Homepage />
+    </>
   )
 }
