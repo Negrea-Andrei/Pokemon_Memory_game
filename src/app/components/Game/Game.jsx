8b46cd2c21d76ca412be87de116f09e6f378d97f
Negrea-Andrei <negrea97@yahoo.com>
2023-11-08 20:15:39 +0200

    Added a function to make the sprites of the pokemon appaire on the screen. When i click on the pokemon they change

--- src/app/components/Game/Game.jsx
+++ src/app/components/Game/Game.jsx
@@ -1,12 +1,41 @@
 import React, { useState } from "react";
-import './Game.css'
+import "./Game.css";
 
-export default function Game() {
+export default function Game({
+  handleDivClick,
+  handleRestartGame,
+  fetchPokemonData,
+  gameOver,
+  start,
+  pokemonData,
+}) {
   const [isFadedIn, setIsFadedIn] = useState(false);
 
   setTimeout(() => {
     setIsFadedIn(true);
-  },100);
+  }, 100);
 
-  return <div className={`test ${isFadedIn ? "fade-in" : ""}`}>Test</div>;
+  return (
+    <div className={`container ${isFadedIn ? "fade-in" : ""}`}>
+      {start ? (
+        <div className="game">
+          <h1>Gen 1 Pokemon</h1>
+          <div className="pokemon-grid">
+            {pokemonData.map((pokemon, index) => (
+              <div
+                key={index}
+                className="pokemon-card"
+                onClick={() => handleDivClick(pokemon.name)}
+              >
+                <img src={pokemon.image} alt={`Pokemon ${index + 1}`} />
+                <p>{pokemon.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        null
+      )}
+    </div>
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,37 +1,165 @@
-"use client"
+"use client";
 import Homepage from "./components/Homepage/Homepage";
 import Game from "./components/Game/Game";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function getRandomPokemon() {
   const gen1Pokemon = [
-    "bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard",
-    "squirtle", "wartortle", "blastoise", "caterpie", "metapod", "butterfree",
-    "weedle", "kakuna", "beedrill", "pidgey", "pidgeotto", "pidgeot",
-    "rattata", "raticate", "spearow", "fearow", "ekans", "arbok",
-    "pikachu", "raichu", "sandshrew", "sandslash", "nidoran-f", "nidorina",
-    "nidoqueen", "nidoran-m", "nidorino", "nidoking", "clefairy", "clefable",
-    "vulpix", "ninetales", "jigglypuff", "wigglytuff", "zubat", "golbat",
-    "oddish", "gloom", "vileplume", "paras", "parasect", "venonat", "venomoth",
-    "diglett", "dugtrio", "meowth", "persian", "psyduck", "golduck", "mankey",
-    "primeape", "growlithe", "arcanine", "poliwag", "poliwhirl", "poliwrath",
-    "abra", "kadabra", "alakazam", "machop", "machoke", "machamp", "bellsprout",
-    "weepinbell", "victreebel", "tentacool", "tentacruel", "geodude", "graveler",
-    "golem", "ponyta", "rapidash", "slowpoke", "slowbro", "magnemite", "magneton",
-    "farfetch'd", "doduo", "dodrio", "seel", "dewgong", "grimer", "muk", "shellder",
-    "cloyster", "gastly", "haunter", "gengar", "onix", "drowzee", "hypno",
-    "krabby", "kingler", "voltorb", "electrode", "exeggcute", "exeggutor", "cubone",
-    "marowak", "hitmonlee", "hitmonchan", "lickitung", "koffing", "weezing",
-    "rhyhorn", "rhydon", "chansey", "tangela", "kangaskhan", "horsea", "seadra",
-    "goldeen", "seaking", "staryu", "starmie", "mr. mime", "scyther", "jynx",
-    "electabuzz", "magmar", "pinsir", "tauros", "magikarp", "gyarados", "lapras",
-    "ditto", "eevee", "vaporeon", "jolteon", "flareon", "porygon", "omanyte",
-    "omastar", "kabuto", "kabutops", "aerodactyl", "snorlax", "articuno",
-    "zapdos", "moltres", "dratini", "dragonair", "dragonite", "mewtwo", "mew"
+    "bulbasaur",
+    "ivysaur",
+    "venusaur",
+    "charmander",
+    "charmeleon",
+    "charizard",
+    "squirtle",
+    "wartortle",
+    "blastoise",
+    "caterpie",
+    "metapod",
+    "butterfree",
+    "weedle",
+    "kakuna",
+    "beedrill",
+    "pidgey",
+    "pidgeotto",
+    "pidgeot",
+    "rattata",
+    "raticate",
+    "spearow",
+    "fearow",
+    "ekans",
+    "arbok",
+    "pikachu",
+    "raichu",
+    "sandshrew",
+    "sandslash",
+    "nidoran-f",
+    "nidorina",
+    "nidoqueen",
+    "nidoran-m",
+    "nidorino",
+    "nidoking",
+    "clefairy",
+    "clefable",
+    "vulpix",
+    "ninetales",
+    "jigglypuff",
+    "wigglytuff",
+    "zubat",
+    "golbat",
+    "oddish",
+    "gloom",
+    "vileplume",
+    "paras",
+    "parasect",
+    "venonat",
+    "venomoth",
+    "diglett",
+    "dugtrio",
+    "meowth",
+    "persian",
+    "psyduck",
+    "golduck",
+    "mankey",
+    "primeape",
+    "growlithe",
+    "arcanine",
+    "poliwag",
+    "poliwhirl",
+    "poliwrath",
+    "abra",
+    "kadabra",
+    "alakazam",
+    "machop",
+    "machoke",
+    "machamp",
+    "bellsprout",
+    "weepinbell",
+    "victreebel",
+    "tentacool",
+    "tentacruel",
+    "geodude",
+    "graveler",
+    "golem",
+    "ponyta",
+    "rapidash",
+    "slowpoke",
+    "slowbro",
+    "magnemite",
+    "magneton",
+    "farfetchd",
+    "doduo",
+    "dodrio",
+    "seel",
+    "dewgong",
+    "grimer",
+    "muk",
+    "shellder",
+    "cloyster",
+    "gastly",
+    "haunter",
+    "gengar",
+    "onix",
+    "drowzee",
+    "hypno",
+    "krabby",
+    "kingler",
+    "voltorb",
+    "electrode",
+    "exeggcute",
+    "exeggutor",
+    "cubone",
+    "marowak",
+    "hitmonlee",
+    "hitmonchan",
+    "lickitung",
+    "koffing",
+    "weezing",
+    "rhyhorn",
+    "rhydon",
+    "chansey",
+    "tangela",
+    "kangaskhan",
+    "horsea",
+    "seadra",
+    "goldeen",
+    "seaking",
+    "staryu",
+    "starmie",
+    "mr-mime",
+    "scyther",
+    "jynx",
+    "electabuzz",
+    "magmar",
+    "pinsir",
+    "tauros",
+    "magikarp",
+    "gyarados",
+    "lapras",
+    "ditto",
+    "eevee",
+    "vaporeon",
+    "jolteon",
+    "flareon",
+    "porygon",
+    "omanyte",
+    "omastar",
+    "kabuto",
+    "kabutops",
+    "aerodactyl",
+    "snorlax",
+    "articuno",
+    "zapdos",
+    "moltres",
+    "dratini",
+    "dragonair",
+    "dragonite",
+    "mewtwo",
+    "mew",
   ];
 
   const randomPokemon = [];
-  while (randomPokemon.length < 10) {
+  while (randomPokemon.length < 15) {
     const randomIndex = Math.floor(Math.random() * gen1Pokemon.length);
     if (!randomPokemon.includes(gen1Pokemon[randomIndex])) {
       randomPokemon.push(gen1Pokemon[randomIndex]);
@@ -41,19 +169,80 @@ function getRandomPokemon() {
   return randomPokemon;
 }
 
-
 export default function App() {
   const [start, setStart] = useState(false);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [clickedPokemonNames, setClickedPokemonNames] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
 
   const startGame = () => {
-    setStart(true)
+    setStart(true);
+  };
+
+  async function fetchPokemonData() {
+    try {
+      const randomPokemon = getRandomPokemon();
+      const pokemonDataPromises = randomPokemon.map(async (pokemon) => {
+        try {
+          const response = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${pokemon}/`
+          );
+          if (!response.ok) {
+            throw Error(`HTTP Error! Status: ${response.status}`);
+          }
+          const data = await response.json();
+          return {
+            name: data.name,
+            image: data.sprites.front_default,
+          };
+        } catch (error) {
+          console.error("Error fetching Pokemon data:", error);
+          return null;
+        }
+      });
+
+      const data = await Promise.all(pokemonDataPromises);
+      setPokemonData(data);
+    } catch (error) {
+      console.error("Error fetching random Pokemon:", error);
+    }
   }
 
-  console.log(getRandomPokemon())
+  useEffect(() => {
+    fetchPokemonData();
+  }, []);
+
+  const handleDivClick = (pokemonName) => {
+    if (!clickedPokemonNames.includes(pokemonName)) {
+      const updatedClickedPokemonNames = [...clickedPokemonNames, pokemonName];
+      setClickedPokemonNames(updatedClickedPokemonNames);
+      fetchPokemonData();
+      console.log(updatedClickedPokemonNames);
+    } else {
+      setGameOver(true);
+    }
+  };
+
+  const handleRestartGame = () => {
+    setClickedPokemonNames([]);
+    setGameOver(false);
+    fetchPokemonData();
+  };
 
   return (
     <div className="container">
-      {start ? <Game /> : <Homepage startGame={startGame} />}
+      {start ? (
+        <Game
+          start={start}
+          gameOver={gameOver}
+          fetchPokemonData={fetchPokemonData}
+          handleDivClick={handleDivClick}
+          handleRestartGame={handleRestartGame}
+          pokemonData={pokemonData}
+        />
+      ) : (
+        <Homepage startGame={startGame} />
+      )}
     </div>
   );
 }
